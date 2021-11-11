3b5b924f8807f085b372ee18a414b8ec8c26e104
omkaarshenoy <omkaarshenoyos@gmail.com>
2021-11-11 01:59:10 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-
-import './App.css';
 import Navbar from './Navbar.js';
-import Home from './Home.js';
+
 
 function App() {
-  const title = 'Memory Glass'
+  const title = "Our vision"
   
   return (
     <div className="App">
       <Navbar/ >
-      <Home />
+      
       <div className="content">
-        <h1>{title}</h1>
+        
+        <h2>{title}</h2>
+        Memory Glass is a startup focused on helping Alzheimers patients reconnect with those they can't remember. Our team is focused on combining technology with people with cognitive memory loss as an alternative to their current prescriptions. The  product combines smart glass technology with facial recognition in order to help identify loved ones of the patients.  
         
       </div>
     </div>
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import Logo from './logo.png';
 
 const Navbar = () => {
     return (
-        <nav className="Navbar">
-            <h1>Memory Glass</h1>
+        <nav className="navbar">
+            <a href="/"> <img src={Logo} alt="Logo" style={{
+                
+                    
+                    width: 240,
+                    height: 126,
+                    
+                
+            }}/> </a>
             <div className="links">
-                <a href="/">Home </a>
-                <a href="/create">New Blog</a>
+                
+                <a href="/aboutus">About us</a>
+                <a href="/login" style= {{
+                    color: "white",
+                    borderRadius: "7px",
+                    backgroundColor: '#6abaf3'
+                }}>Login</a>
+                
             </div>
         </nav>
     );
