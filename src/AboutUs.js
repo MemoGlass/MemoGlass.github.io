9599e4ad9f9c9e9fa6e86ce708492fb2f19611be
omkaarshenoy <omkaarshenoyos@gmail.com>
2021-11-11 18:03:18 -0700

    Added About Page

---
+++ src/AboutUs.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+const AboutUs = () => {
+    const [about, setAbout] = useState([
+        { title: 'Pitch 📊', body: 'Memory glass is a small standalone device that uses digital projection and facial recognition to help Alzheimer’s patients remember their loved ones.', id: 1 },
+        { title: 'Problem/Oppurtunity 🤔', body: 'Memory Glass is aimed at patients with cognitive diseases. These individuals are generally an older demographic, so we have a large focus on making the design intuitive. Also, 80% of people over the age of 65 already wear glasses, allowing them to adopt our device into their lives with very few issues.', id: 2 },
+        { title: 'Solution/Product 💡', body: 'Our device will enhance a patient’s quality of life by instantly reconnecting them to their families. It provides facial recognition to minimize stress and frustration. Memory glass also promotes social interaction within care environments between other patients or caregivers.', id: 3 },
+        { title: 'Business Model 📈', body: 'Memory Glass works as a SAAS model and leased hardware Business model. The software and hardware will cost customers $50/month and provide monthly profit of 20$/month, a 40% margin.', id: 4 },
+        { title: 'Market 💰', body: 'Alzheimer’s includes 6 million in the U.S market. We will market to doctors, caregivers, and Alzheimer’s family members. At 1% market penetration, the yearly revenue equates to $36 million.', id: 5 },
+        { title: 'Competition 🥊', body: 'Memory Glass is a novel concept for Alzheimer’s and has no direct competitors. Although there are companies focused on smart glasses, they do not include specific features tailored to Alzheimer’s.', id: 6 },
+        { title: 'Unique Features 🦄', body: 'Memory Glass is an attachment to increase modularity and reduce cost. We use cloud-based facial recognition to dramatically increase battery life and processing power of the device. Finally, it utilizes built in 5G communication to efficiently transmit relevant data.', id: 7 },
+        { title: 'Go to Market Strategy 🤵', body: 'Upon diagnosis of Alzheimer’s, family members will be provided an opportunity to purchase Memory Glass for their loved ones. It will be advertised through doctors and associations. Memory is just as important to the family as it is to the patient.', id: 8 },
+        { title: 'Traction 🤝', body: 'We are sending out surveys to nursing homes, doctors, patients and family members to gather valuable feedback and validation. Engineering has provided a finished prototype which will be used for small scale testing and refinement.', id: 9 },
+
+    ])
+    return (
+        <div className="AboutUs">
+            <h1>About MemoryGlass</h1>
+            {about.map((about) => (
+
+                <div className="about" key={about.id}>
+                    
+                    <h2>{about.title}</h2>
+                    <p>{about.body}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default AboutUs;
--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import Navbar from './Navbar.js';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Home.js';
+import Login from './Login.js';
+import AboutUs from './AboutUs.js';
 
 
 function App() {
-  const title = "Our vision"
   
+
   return (
-    <div className="App">
-      <Navbar/ >
-      <h1>MemoryGlass</h1>
-      <div className="content">
+    <Router>
+      <div className="App">
+        <Navbar />
         
-        <h2>{title}</h2>
-        Memory Glass is a startup focused on helping Alzheimers patients reconnect with those they can't remember. Our team is focused on combining technology with people with cognitive memory loss as an alternative to their current prescriptions. The  product combines smart glass technology with facial recognition in order to help identify loved ones of the patients.  
-        
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+            <Route exact path="/aboutus">
+              <AboutUs />
+            </Route>
+
+          </Switch>
+          
+
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
