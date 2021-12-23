e3bc2062802e42e2c3fa33106d66cf77506e04bd
Omkaar Shenoy <omkaarshenoyos@gmail.com>
2021-12-22 20:45:04 -0800

    team page edit

--- src/Team.js
+++ src/Team.js
@@ -1,53 +1,34 @@
 import { useState } from 'react';
 import Navbar from './Navbar.js'
-import { Button, Card , CardGroup} from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 import Dog from './dog.png'
 const Team = () => {
     const [team] = useState([
-        { name: 'John', position: 'Chief Exec Officer' },
-        { name: 'Andrew', position: 'Team Member' }, 
+        { name: 'Andrew', position: 'Chief Exec Officer' },
+        { name: 'Omkaar', position: 'Software Team Member' }, 
+        { name: 'Omkaar', position: 'Software Team Member' },
     ])
     return (
         <div>
             <Navbar />
             <div className='content' >
                     <h1>The Memory Glass Team</h1>
+                    <div style={{display:'flex', flexWrap: 'wrap'}}>
                     {team.map((team) => (
                         <div className="team">
-                            <CardGroup>
-                            <Card style={{ width: '18rem', margin:'10px' }}>
-                                <Card.Img variant="top" src={Dog} style={{ borderRadius: '50%' }} />
-                                <Card.Body>
-                                    <Card.Title><h2>Andrew Deros</h2></Card.Title>
-                                    <Card.Text style={{ marginTop: '-30px', paddingBottom: '30px' , marginLeft:'20px'}}>
-                                        <h3>Chief Exec Officer</h3>
-                                        <Button variant="primary" style={{ position: 'absolute', left: '42%', top: '88%', }}><i className="fa fa-envelope" /></Button>
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                            <Card style={{ width: '18rem' , margin:'10px'}}>
-                                <Card.Img variant="top" src={Dog} style={{ borderRadius: '50%' }} />
-                                <Card.Body>
-                                    <Card.Title><h2>Omkaar Shenoy</h2></Card.Title>
-                                    <Card.Text style={{ marginTop: '-30px', paddingBottom: '30px', marginLeft:'12px', fontSize:'15px'}}>
-                                        <h3>Software Team Member</h3>
-                                        <Button variant="primary" style={{ position: 'absolute', left: '42%', top: '88%', }}><i className="fa fa-envelope" /></Button>
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                            <Card style={{ width: '18rem' , margin:'10px'}}>
+                            <Card style={{ width: '18rem', margin: '10px' }}>
                                 <Card.Img variant="top" src={Dog} style={{ borderRadius: '50%' }} />
                                 <Card.Body>
                                     <Card.Title><h2>{team.name}</h2></Card.Title>
-                                    <Card.Text style={{ marginTop: '-30px', paddingBottom: '30px' }}>
+                                    <Card.Text style={{ marginTop: '-30px', paddingBottom: '30px', fontSize:'15px' }}>
                                         <h3>{team.position}</h3>
                                         <Button variant="primary" style={{ position: 'absolute', left: '42%', top: '88%', }}><i className="fa fa-envelope" /></Button>
                                     </Card.Text>
                                 </Card.Body>
                             </Card>
-                            </CardGroup>
                         </div>
                     ))}
+                    </div>
             </div>
         </div>
     );
