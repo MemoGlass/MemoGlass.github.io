184bc9c2e485ffbd11122811f03f1c99befeda9b
Omkaar Shenoy <omkaarshenoyos@gmail.com>
2022-04-13 17:39:07 -0700

    added reminders page

--- src/Settings.js
+++ src/Settings.js
@@ -3,7 +3,7 @@ const Settings = () => {
         <div>
             <h1>Settings</h1>
             <div style={{display:'flex', align:'centre'}}>
-                <button style={{display:'flex', marginLeft:'127px'}}>Change Projection</button>
+                <button style={{display:'flex', marginLeft:'127px', }}>Change Projection</button>
                 
                 <button style={{display:'flex', marginLeft:'127px'}}> Connect to Glass</button>
                 <button style={{display:'flex', marginLeft:'127px'}}>Privacy Settings</button>
--- src/reminders.js
+++ src/reminders.js
@@ -1,9 +1,57 @@
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import { Link } from 'react-router-dom';
+
 const Reminder = () => {
-    return ( 
-        <div className="ralign">
-            Take Medicine 12:00
+    return (
+        <div>
+                 <Link to ="/login" style= {{
+                    color: "white",
+                    borderRadius: "7px",
+                    backgroundColor: '#6abaf3',
+                    
+                    marginLeft: '845px',
+       
+                    padding: '10px'
+                }}>+ New Reminder</Link>
+                <div>&nbsp;</div>
+        <div className="box">
+            <div className="lalign">
+                Take Medicine
+            </div>
+            <div className="ralign">
+            <i class="fa-solid fa-sliders-simple"></i>
+            
+            <i class="fa fa-toggle-on"></i>
+            {'                  ' }&nbsp;
+            12:00
+            </div>
         </div>
-     );
+        <div className="box">
+            <div className="lalign">
+                Water the plants
+            </div>
+            <div className="ralign">
+            <i class="fa-solid fa-sliders-simple"></i>
+            
+            <i class="fa fa-toggle-on"></i>
+            {'                     ' }{'                     ' }&nbsp;
+            17:00
+            </div>
+        </div>
+        <div className="box">
+            <div className="lalign">
+                Feed the dog
+            </div>
+            <div className="ralign">
+            <i class="fa-solid fa-sliders-simple"></i>
+            
+            <i class="fa fa-toggle-on"></i>
+            {'                  ' }&nbsp;
+            20:00
+            </div>
+        </div>
+        </div>
+    );
 }
- 
+
 export default Reminder;
